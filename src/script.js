fd9c356ce8d00e0d5729c4ea0c0f4230b415727e
Agustinusmulianto <agustinus.yan96@gmail.com>
2024-05-16 12:47:00 +0700

    Revisi 1

--- src/script.js
+++ src/script.js
@@ -44,3 +44,67 @@ function validateFormAkademik() {
     }
     return true;
 }
+
+function tableDetail() {
+    let open = document.getElementById("open-detail");
+    let close = document.getElementById("close-detail");
+    let table = document.getElementById("detail-table");
+
+    if (open) {
+        open.addEventListener('click', function () {
+            table.style.display = 'block';
+            open.style.display = 'none';
+            close.style.display = 'block';
+        });
+    }
+
+    if (close) {
+        close.addEventListener('click', function () {
+            table.style.display = 'none';
+            close.style.display = 'none';
+            open.style.display = 'block';
+        });
+    }
+}
+
+// Call the function to set up the event listeners
+tableDetail();
+
+function tableDetail2() {
+    let open2 = document.getElementById("open-detail-2");
+    let close2 = document.getElementById("close-detail-2");
+    let table2 = document.getElementById("detail-table-2");
+
+    if (open2) {
+        open2.addEventListener('click', function () {
+            table2.style.display = 'block';
+            open2.style.display = 'none';
+            close2.style.display = 'block';
+        });
+    }
+
+    if (close2) {
+        close2.addEventListener('click', function () {
+            table2.style.display = 'none';
+            close2.style.display = 'none';
+            open2.style.display = 'block';
+        });
+    }
+}
+
+// Call the function to set up the event listeners
+tableDetail2();
+
+function previewImage(event) {
+    const file = event.target.files[0];
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function (e) {
+            const imagePreview = document.getElementById('image-preview');
+            const imagePreviewImg = document.getElementById('image-preview-img');
+            imagePreviewImg.src = e.target.result;
+            imagePreview.classList.remove('hidden');
+        };
+        reader.readAsDataURL(file);
+    }
+}
